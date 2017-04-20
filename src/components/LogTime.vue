5e287a1b5f165c3366b0d980b0d99afe17251ec0
Zhang Yusen <zysallen@users.noreply.github.com>
2017-04-20 21:08:44 +0800

    Update LogTime.vue

--- src/components/LogTime.vue
+++ src/components/LogTime.vue
@@ -52,7 +52,7 @@
         methods:{
           save() {
             const plan = {
-              name : '二哲',
+              name : 'zys',
               image : 'https://sfault-avatar.b0.upaiyun.com/888/223/888223038-5646dbc28d530_huge256',
               date : this.date,
               totalTime : this.totalTime,
